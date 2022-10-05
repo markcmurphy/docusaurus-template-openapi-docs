4d65ecaa04ea4d397f598317f01028c2ad19e988
Mark <mark@murphymark.me>
2022-10-05 16:23:40 -0500

    initial site

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,50 +1,50 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require("prism-react-renderer/themes/github");
-const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "My Site",
-  tagline: "Dinosaurs are cool",
-  url: "https://your-docusaurus-test-site.com",
-  baseUrl: "/",
-  onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
-  favicon: "img/favicon.ico",
+  title: 'BC Dev Center',
+  tagline: 'BigCommerce Developer Center',
+  url: 'https://developer.bigcommerce.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  // favicon: 'img/favicon.ico',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: 'bigcommerce', // Usually your GitHub org/user name.
+  projectName: 'dev-docs', // Usually your repo name.
 
   presets: [
     [
-      "classic",
+      'classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve("./sidebars.js"),
+          sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
-          docLayoutComponent: "@theme/DocPage",
-          docItemComponent: "@theme/ApiItem" // Derived from docusaurus-theme-openapi
+            'https://github.com/bigcommerce/dev-docs/tree/master/docs/api-docs',
+          docLayoutComponent: '@theme/DocPage',
+          docItemComponent: '@theme/ApiItem', // Derived from docusaurus-theme-openapi
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/"
-        },
+        // blog: {
+        //   showReadingTime: true,
+        //   // Please change this to your repo.
+        //   // Remove this to remove the "edit this page" links.
+        //   editUrl:
+        //     'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        // },
         theme: {
-          customCss: require.resolve("./src/css/custom.css")
-        }
-      })
-    ]
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
   ],
 
   themeConfig:
@@ -52,108 +52,136 @@ const config = {
     ({
       docs: {
         sidebar: {
-          hideable: true
-        }
+          hideable: true,
+        },
       },
       navbar: {
-        title: "My Site",
+        title: 'Dev Center',
         logo: {
-          alt: "My Site Logo",
-          src: "img/logo.svg"
+          alt: 'My Site Logo',
+          src: 'img/BigCommerce-logomark-whitebg.png',
         },
         items: [
           {
-            type: "doc",
-            docId: "intro",
-            position: "left",
-            label: "Tutorial"
-          },
-          { to: "/blog", label: "Blog", position: "left" },
-          {
-            label: "Petstore API",
-            position: "left",
-            to: "/docs/category/petstore-api"
+            type: 'docSidebar',
+            sidebarId: 'docsSidebar',
+            position: 'left',
+            label: 'Docs',
           },
           {
-            href: "https://github.com/facebook/docusaurus",
-            label: "GitHub",
-            position: "right"
-          }
-        ]
+            type: 'docSidebar',
+            sidebarId: 'apiSpecSidebar',
+            position: 'left',
+            label: 'API Specs',
+          },
+          // { to: '/blog', label: 'Blog', position: 'left' },
+          // {
+          //   label: 'Petstore API',
+          //   position: 'left',
+          //   to: 'docs/category/petstore-api',
+          // },
+          // {
+          //   label: 'Checkouts',
+          //   position: 'left',
+          //   to: 'docs/category/checkouts',
+          // },
+          {
+            href: 'https://github.com/bigcommerce/dev-docs',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
       },
       footer: {
-        style: "dark",
-        links: [
-          {
-            title: "Docs",
-            items: [
-              {
-                label: "Tutorial",
-                to: "/docs/intro"
-              }
-            ]
-          },
-          {
-            title: "Community",
-            items: [
-              {
-                label: "Stack Overflow",
-                href: "https://stackoverflow.com/questions/tagged/docusaurus"
-              },
-              {
-                label: "Discord",
-                href: "https://discordapp.com/invite/docusaurus"
-              },
-              {
-                label: "Twitter",
-                href: "https://twitter.com/docusaurus"
-              }
-            ]
-          },
-          {
-            title: "More",
-            items: [
-              {
-                label: "Blog",
-                to: "/blog"
-              },
-              {
-                label: "GitHub",
-                href: "https://github.com/facebook/docusaurus"
-              }
-            ]
-          }
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`
+        style: 'dark',
+        // links: [
+        //   {
+        //     title: 'Docs',
+        //     items: [
+        //       {
+        //         label: 'Tutorial',
+        //         to: '/docs/intro',
+        //       },
+        //     ],
+        //   },
+        //   {
+        //     title: 'Community',
+        //     items: [
+        //       {
+        //         label: 'Stack Overflow',
+        //         href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+        //       },
+        //       {
+        //         label: 'Discord',
+        //         href: 'https://discordapp.com/invite/docusaurus',
+        //       },
+        //       {
+        //         label: 'Twitter',
+        //         href: 'https://twitter.com/docusaurus',
+        //       },
+        //     ],
+        //   },
+        //   {
+        //     title: 'More',
+        //     items: [
+        //       {
+        //         label: 'Blog',
+        //         to: '/blog',
+        //       },
+        //       {
+        //         label: 'GitHub',
+        //         href: 'https://github.com/facebook/docusaurus',
+        //       },
+        //     ],
+        //   },
+        // ],
+        copyright: `Copyright © ${new Date().getFullYear()}`,
       },
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
-        additionalLanguages: ["ruby", "csharp", "php"]
-      }
+        additionalLanguages: ['ruby', 'csharp', 'php'],
+      },
     }),
 
   plugins: [
     [
-      "docusaurus-plugin-openapi-docs",
+      'docusaurus-plugin-openapi-docs',
       {
-        id: "openapi",
-        docsPluginId: "classic",
+        id: 'openapi',
+        docsPluginId: 'classic',
         config: {
           petstore: {
-            specPath: "examples/petstore.yaml",
-            outputDir: "docs/petstore",
+            specPath: 'examples/petstore.yaml',
+            outputDir: 'docs/petstore',
             sidebarOptions: {
-              groupPathsBy: "tag",
-              categoryLinkSource: "tag"
-            }
-          }
-        }
-      }
-    ]
+              groupPathsBy: 'tag',
+              categoryLinkSource: 'tag',
+            },
+          },
+          checkouts: {
+            specPath: 'specs/checkouts.v3.yml',
+            outputDir: 'docs/checkouts',
+            sidebarOptions: {
+              groupPathsBy: 'tag',
+              categoryLinkSource: 'tag',
+            },
+          },
+          customers: {
+            specPath: 'specs/customers.v3.yaml',
+            outputDir: 'docs/customers',
+            sidebarOptions: {
+              groupPathsBy: 'tag',
+              categoryLinkSource: 'tag',
+            },
+            // template: 'api.mustache', // Customize API MDX with mustache template
+          },
+        },
+      },
+    ],
   ],
 
-  themes: ["docusaurus-theme-openapi-docs"]
+  themes: ['docusaurus-theme-openapi-docs'],
 };
 
 module.exports = config;
